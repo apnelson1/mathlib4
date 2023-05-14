c1b19e8865989bc26b1b38a0ccfad091e2d6cfd6
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-05-13 20:07:14 -0400

    some golfs

--- Mathlib/Analysis/NormedSpace/Basic.lean
+++ Mathlib/Analysis/NormedSpace/Basic.lean
@@ -92,7 +92,7 @@ theorem norm_smul [NormedSpace α β] (s : α) (x : β) : ‖s • x‖ = ‖s�
   · refine' le_antisymm (norm_smul_le s x) _
     calc
       ‖s‖ * ‖x‖ = ‖s‖ * ‖s⁻¹ • s • x‖ := by rw [inv_smul_smul₀ h]
-      _ ≤ ‖s‖ * (‖s⁻¹‖ * ‖s • x‖) := (mul_le_mul_of_nonneg_left (norm_smul_le _ _) (norm_nonneg _))
+      _ ≤ ‖s‖ * (‖s⁻¹‖ * ‖s • x‖) := by rel_congr; apply norm_smul_le
       _ = ‖s • x‖ := by rw [norm_inv, ← mul_assoc, mul_inv_cancel (mt norm_eq_zero.1 h), one_mul]
 #align norm_smul norm_smul
 
