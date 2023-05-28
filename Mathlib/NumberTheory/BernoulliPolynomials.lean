766481d58c2abacda7b16da543baf866b1c3a34d
adomani <adomani@gmail.com>
2023-05-28 14:08:45 +0200

    Fix `cases` with `cases n with | ... | ...`

--- Mathlib/NumberTheory/BernoulliPolynomials.lean
+++ Mathlib/NumberTheory/BernoulliPolynomials.lean
@@ -231,41 +231,42 @@ theorem bernoulli_generating_function (t : A) :
   -- check equality of power series by checking coefficients of X^n
   ext n
   -- n = 0 case solved by `simp`
-  cases n;
-  · simp
+  cases n with
+  | zero => simp
   -- n ≥ 1, the coefficients is a sum to n+2, so use `sum_range_succ` to write as
   -- last term plus sum to n+1
-  rw [coeff_succ_X_mul, coeff_rescale, coeff_exp, PowerSeries.coeff_mul,
-    Nat.sum_antidiagonal_eq_sum_range_succ_mk, sum_range_succ]
-  -- last term is zero so kill with `add_zero`
-  simp only [RingHom.map_sub, tsub_self, constantCoeff_one, constantCoeff_exp,
-    coeff_zero_eq_constantCoeff, MulZeroClass.mul_zero, sub_self, add_zero]
-  -- Let's multiply both sides by (n+1)! (OK because it's a unit)
-  have hnp1 : IsUnit ((n + 1)! : ℚ) := IsUnit.mk0 _ (by exact_mod_cast factorial_ne_zero (n + 1))
-  rw [← (hnp1.map (algebraMap ℚ A)).mul_right_inj]
-  -- do trivial rearrangements to make RHS (n+1)*t^n
-  rw [mul_left_comm, ← RingHom.map_mul]
-  change _ = t ^ n * algebraMap ℚ A (((n + 1) * n ! : ℕ) * (1 / n !))
-  rw [cast_mul, mul_assoc,
-    mul_one_div_cancel (show (n ! : ℚ) ≠ 0 from cast_ne_zero.2 (factorial_ne_zero n)), mul_one,
-    mul_comm (t ^ n), ← aeval_monomial, cast_add, cast_one]
-  -- But this is the RHS of `sum_bernoulli_poly`
-  rw [← sum_bernoulli, Finset.mul_sum, AlgHom.map_sum]
-  -- and now we have to prove a sum is a sum, but all the terms are equal.
-  apply Finset.sum_congr rfl
-  -- The rest is just trivialities, hampered by the fact that we're coercing
-  -- factorials and binomial coefficients between ℕ and ℚ and A.
-  intro i hi
-  -- deal with coefficients of e^X-1
-  simp only [Nat.cast_choose ℚ (mem_range_le hi), coeff_mk, if_neg (mem_range_sub_ne_zero hi),
-    one_div, AlgHom.map_smul, PowerSeries.coeff_one, coeff_exp, sub_zero, LinearMap.map_sub,
-    Algebra.smul_mul_assoc, Algebra.smul_def, mul_right_comm _ ((aeval t) _), ← mul_assoc, ←
-    RingHom.map_mul, succ_eq_add_one, ← Polynomial.C_eq_algebraMap, Polynomial.aeval_mul,
-    Polynomial.aeval_C]
-  -- finally cancel the Bernoulli polynomial and the algebra_map
-  congr
-  apply congr_arg
-  rw [mul_assoc, div_eq_mul_inv, ← mul_inv]
+  | succ n =>
+    rw [coeff_succ_X_mul, coeff_rescale, coeff_exp, PowerSeries.coeff_mul,
+      Nat.sum_antidiagonal_eq_sum_range_succ_mk, sum_range_succ]
+    -- last term is zero so kill with `add_zero`
+    simp only [RingHom.map_sub, tsub_self, constantCoeff_one, constantCoeff_exp,
+      coeff_zero_eq_constantCoeff, MulZeroClass.mul_zero, sub_self, add_zero]
+    -- Let's multiply both sides by (n+1)! (OK because it's a unit)
+    have hnp1 : IsUnit ((n + 1)! : ℚ) := IsUnit.mk0 _ (by exact_mod_cast factorial_ne_zero (n + 1))
+    rw [← (hnp1.map (algebraMap ℚ A)).mul_right_inj]
+    -- do trivial rearrangements to make RHS (n+1)*t^n
+    rw [mul_left_comm, ← RingHom.map_mul]
+    change _ = t ^ n * algebraMap ℚ A (((n + 1) * n ! : ℕ) * (1 / n !))
+    rw [cast_mul, mul_assoc,
+      mul_one_div_cancel (show (n ! : ℚ) ≠ 0 from cast_ne_zero.2 (factorial_ne_zero n)), mul_one,
+      mul_comm (t ^ n), ← aeval_monomial, cast_add, cast_one]
+    -- But this is the RHS of `sum_bernoulli_poly`
+    rw [← sum_bernoulli, Finset.mul_sum, AlgHom.map_sum]
+    -- and now we have to prove a sum is a sum, but all the terms are equal.
+    apply Finset.sum_congr rfl
+    -- The rest is just trivialities, hampered by the fact that we're coercing
+    -- factorials and binomial coefficients between ℕ and ℚ and A.
+    intro i hi
+    -- deal with coefficients of e^X-1
+    simp only [Nat.cast_choose ℚ (mem_range_le hi), coeff_mk, if_neg (mem_range_sub_ne_zero hi),
+      one_div, AlgHom.map_smul, PowerSeries.coeff_one, coeff_exp, sub_zero, LinearMap.map_sub,
+      Algebra.smul_mul_assoc, Algebra.smul_def, mul_right_comm _ ((aeval t) _), ← mul_assoc, ←
+      RingHom.map_mul, succ_eq_add_one, ← Polynomial.C_eq_algebraMap, Polynomial.aeval_mul,
+      Polynomial.aeval_C]
+    -- finally cancel the Bernoulli polynomial and the algebra_map
+    congr 1  -- porting note: was `congr`
+    apply congr_arg
+    rw [mul_assoc, div_eq_mul_inv, ← mul_inv]
 #align polynomial.bernoulli_generating_function Polynomial.bernoulli_generating_function
 
 end Polynomial
